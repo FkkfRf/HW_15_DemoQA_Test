bec8be4bb8a50cbf22514a8fa707d58089c7d319
FkkfRf <Fkkf.Rf@gmail.com>
2022-09-21 23:33:02 +0300

    Fill ALL Fields

--- src/test/java/com/DemoQA/TextBoxTests.java
+++ src/test/java/com/DemoQA/TextBoxTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -27,16 +28,24 @@ public class TextBoxTests {
         $("#firstName").setValue("Nимя");
         $("#lastName").setValue(" Фамили");
         $("#userEmail").setValue("User@email.com");
-        $(By.name("gender")).selectRadio("Male").click();
-        $("#gender-radio-3").click();
-        $("#userNumber").setValue("8999000001");
+        $(byText("Male")).click();
+        $(byText("Female")).click();
+        $(byText("Other")).click();
+        $("#userNumber").setValue("8299000001");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(By.className("react-datepicker__year-select")).selectOption("1975");
+        $(".react-datepicker__day--029:not(.react-datepicker__day--outside-month").click();
         $("#uploadPicture").uploadFromClasspath("ForDemoQA.txt");
         $("#subjectsInput").setValue("a").pressEnter().setValue("i").pressEnter().setValue("m").pressEnter();
+        $(byText("Sports")).click();
+        $(byText("Music")).click();
+        $(byText("Reading")).click();
         $("#currentAddress").setValue(" 221b \n Baker St, \n  London, Grate Britain ");
-        $("#dateOfBirthInput").click();
-        //$("#subjectsInput").selectOption("May");
-        $("#submit").click();
+        $("#stateCity-wrapper").click();
+        $(byText("Haryana")).click();
+        $(byText("Select City")).click();
+        $(byText("Karnal")).click();
+        $(byText("Submit")).click();
     }
-
-
 }
