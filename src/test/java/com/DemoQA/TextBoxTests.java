4a8d2e5c47d10531b15e509495329d6d784a2a57
FkkfRf <Fkkf.Rf@gmail.com>
2022-09-22 23:23:39 +0300

    Test all

--- src/test/java/com/DemoQA/TextBoxTests.java
+++ src/test/java/com/DemoQA/TextBoxTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -25,27 +27,66 @@ public class TextBoxTests {
     @Test
     void fillFormTest(){
         open("/automation-practice-form");
-        $("#firstName").setValue("Nимя");
-        $("#lastName").setValue(" Фамили");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        //____убрать рекламу_________________пометка для себя_
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        //________________________________________________
+
+        $("#firstName").setValue("StudentName ");
+        $("#lastName").setValue(" StudentLastName");
         $("#userEmail").setValue("User@email.com");
-        $(byText("Male")).click();
-        $(byText("Female")).click();
-        $(byText("Other")).click();
+
+        $("#gender-radio-1").parent().click();
+        $("#genterWrapper").$(byText("Female")).click();
+
         $("#userNumber").setValue("8299000001");
+
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
         $(By.className("react-datepicker__year-select")).selectOption("1975");
         $(".react-datepicker__day--029:not(.react-datepicker__day--outside-month").click();
+
         $("#uploadPicture").uploadFromClasspath("ForDemoQA.txt");
+
         $("#subjectsInput").setValue("a").pressEnter().setValue("i").pressEnter().setValue("m").pressEnter();
-        $(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
         $(byText("Music")).click();
-        $(byText("Reading")).click();
+        $(by("for","hobbies-checkbox-2")).click();
+
         $("#currentAddress").setValue(" 221b \n Baker St, \n  London, Grate Britain ");
-        $("#stateCity-wrapper").click();
+        $("#state").click();
         $(byText("Haryana")).click();
-        $(byText("Select City")).click();
+        $("#city").click();
         $(byText("Karnal")).click();
-        $(byText("Submit")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave((text("StudentName StudentLastName")));
+        $(".table-responsive").$(byText("Student Email"))
+                .parent().shouldHave((text("User@email.com")));
+        $(".table-responsive").$(byText("Gender"))
+                .parent().shouldHave((text("Female")));
+        $(".table-responsive").$(byText("Mobile"))
+                .parent().shouldHave((text("8299000001")));
+        $(".table-responsive").$(byText("Date of Birth"))
+                .parent().shouldHave((text("29 May,1975")));
+        $(".table-responsive").$(byText("Subjects"))
+                .parent().shouldHave((text("Maths, Hindi, Chemistry")));
+        $(".table-responsive").$(byText("Hobbies"))
+                .parent().shouldHave((text("Sports, Music, Reading")));
+        $(".table-responsive").$(byText("Picture"))
+                .parent().shouldHave((text("ForDemoQA.txt")));
+        $(".table-responsive").$(byText("Address"))
+                .parent().shouldHave((text("221b Baker St, London, Grate Britain")));
+        $(".table-responsive").$(byText("State and City"))
+                .parent().shouldHave((text("Haryana Karnal")));
     }
 }
+
+
+
